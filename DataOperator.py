8459473260793062b0eb7d1f99a9b025ab06a2a1
TFI\ntani <ntani@techfirm.co.jp>
2018-08-29 13:40:42 +0900

    userテーブル作成コマンドを追加

--- DataOperator.py
+++ DataOperator.py
@@ -7,11 +7,13 @@ class DataOperator:
   def __init__(self):
     # 定数の設定
     self.abc_path = 'data/abc'
-    self.sql_path = 'data'
+    self.db_path = 'data'
     self.dbname = 'lacerta.db'
     self.user_tbl_name = 'user_tbl'
     # カーソルの設定
-    self.user_cur = self.get_user_cursor()
+    self.user_cur = self.connect_db()
+    # userテーブルの作成
+    self.create_user_tbl()
   
   # jsonファイルの読み込み
   def read_json(self, num):
@@ -19,41 +21,23 @@ class DataOperator:
       json_dict = json.load(f)
     return json_dict
   
-  # dbの作成、カーソルの取得
-  def get_user_cursor(self):
+  # dbへの接続
+  def connect_db(self):
+    return sqlite3.connect('{}/{}'.format(self.db_path, self.dbname)).cursor()
+  
+  # テーブルの作成
+  def create_user_tbl(self):
     sql_createdb = """
       CREATE TABLE IF NOT EXISTS {}(
         id int primary key,
         uname text,
         uname_screen text,
-        twitter_id, text,
+        twitter_id text,
         coutry text,
         rating int,
         competitions int
-      );
-    """.format(self.user_tbl_name)
-    return sqlite3.connect(self.user_tbl_name).cursor()
+      );""".format(self.user_tbl_name)
+    self.user_cur.execute(sql_createdb)
 
 
 a = DataOperator()
-# for d in a.read_json(100)['data']:
-#   # print(d)
-#   for j in d['tasks']:
-#     print(j)
-# print(a.read_json(1)['data'][0])
-
-# conn = sqlite3.connect('data/example.db')
-# c = conn.cursor()
-# # Create table
-# c.execute('''CREATE TABLE stocks
-#              (date text, trans text, symbol text, qty real, price real)''')
-#
-# # Insert a row of data
-# c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
-#
-# # Save (commit) the changes
-# conn.commit()
-#
-# # We can also close the connection if we are done with it.
-# # Just be sure any changes have been committed or they will be lost.
-# conn.close()
